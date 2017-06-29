a414f4430f2d4810865cbd100828372ba76c6ca2
Bryn Bayliss <brynbayliss@hotmail.com>
2017-06-29 20:42:45 +0000

    Initial commit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc(temp)
+
+  (temp -32)*5/9
+
+end
+
+def ctof(temp)
+
+  temp.to_f*9/5+32
+
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,44 @@
-#write your code here
+def add(arg1, arg2)
+
+  arg1 + arg2
+
+end
+
+
+def subtract(arg1, arg2)
+
+  arg1 - arg2
+
+end
+
+def sum(arr)
+  
+  total =0
+
+  if arr.empty?
+   total = 0 
+  elsif arr.length == 1
+    total = arr[0] 
+  elsif arr.length == 2
+    total = arr[0] + arr[1]
+  else
+    arr.each do  |i|
+      total+=i
+    end
+  end
+  total
+end
+
+def multiply(arr)
+
+  total = 0
+
+  if arr.length == 2
+    total = arr[0] * arr[1]
+  else
+    arr.each do |i|
+      total *= i
+    end
+  end
+  total
+end
