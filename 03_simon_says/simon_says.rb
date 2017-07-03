5052865675e0d27ee6699648cb3fe9e81b338eba
Bryn Bayliss <brynbayliss@hotmail.com>
2017-07-03 18:12:25 +0100

    wrote echo, shout and repeat method to pass tests

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,15 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, numrepeats = 2)
+  result = ""
+  numrepeats.times do
+    result += (string + " ")
+  end
+  result.chomp(" ")
+end
