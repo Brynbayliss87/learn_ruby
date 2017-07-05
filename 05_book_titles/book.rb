3fd26499e82e405d5433da5922242dcf76a03f7f
Bryn Bayliss <brynbayliss@hotmail.com>
2017-07-06 00:45:19 +0100

    Create methods for the Book class to pass specs

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+
+  attr_accessor :title
+  
+  def intialize(title)
+    @title = title
+  end
+    
+  def title
+    words = @title.split()
+    word_array = []
+    words.each do |word|
+      unless (word == "and") || (word == "of") || (word == "the") || (word == "in") || (word == "a") || (word == "an")
+        word_array << word.capitalize
+      else
+        word_array << word.downcase
+      end
+    end
+    @title = word_array.join(" ")
+    @title[0] = @title[0].upcase
+    @title
+  end
 end
