6fba989017f2e84045fc3edd0030303da6d1f35f
Bryn Bayliss <brynbayliss@hotmail.com>
2017-07-05 22:17:23 +0100

    pig latin method broken passing third test

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,25 @@
 def translate(string)
-  if string =~ /\b[aeiou]\w*/i 
-    string << "ay"
-  elsif string=~ /\b[^aeiou]\w*/i
-    string.prepend("ay")
+  result=""
+  string = string.split(" ")
+  string.each do |word|
+    if word =~ /\b[aeiou]\w*/i 
+      word += "ay"
+      result += "#{word} "
+    else
+      if word[1] =~ /\b[aeiou]\w*/i
+        char = word.slice!(0)
+        word += "#{char}ay"
+        result += "#{word} "
+      elsif word[2] =~/\b[aeiou]\w*/i
+        char = string.slice!(0, 2)
+        word += "#{char}ay"
+        result += "#{word} "
+      else char = string.slice!(0, 3)
+        word += "#{char}ay"
+        result += "#{word} "
+      end
+    end
   end
+  result.chomp(" ")
 end
 
